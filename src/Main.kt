f59cf66c4cd21016668d912c37face0558174ae6
waimea-mewingate <mewingate@waimea.school.nz>
2026-03-19 12:25:08 +1300

    STarted coding

--- src/Main.kt
+++ src/Main.kt
@@ -11,9 +11,29 @@
  * =====================================================================
  */
 
-
+val gameBoard = mutableListOf<String>()
 
 fun main() {
-    println("Hello World!")
+    //Filling the list
+    repeat(16) {
+        gameBoard.add("..")
+    }
+    //Welcome screen and counter setup on the board
+    println("Welcome to Pinned!")
+    println("This is a two-player game. ")
+    showGameBoard()
+    val players = listOf("Player One, Player Two")
+    // Googled how to do a random index
+    val whoStarts = players.indices.random()
+    println("Starting Player: ${players[whoStarts]}")
+    //hmm, prints both players
 }
 
+fun showGameBoard() {
+    println("╒═══╤═══╤═══╤═══╤═══╤═══╤═══╤═══╤═══╤═══╤═══╤═══╤═══╤═══╤═══╤═══╕")
+    for (i in 0..15) {
+        print("│"); print(gameBoard[i].padEnd(3))
+    }
+    println("│")
+    println("╘═══╧═══╧═══╧═══╧═══╧═══╧═══╧═══╧═══╧═══╧═══╧═══╧═══╧═══╧═══╧═══╛")
+}
